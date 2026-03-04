c606a01c5de4ccb2703ac3c5d0da06543a37481c
James Mata <jamesmata556@gmail.com>
2026-03-04 15:30:03 -0500

    Recipe screen established

--- rankmyroast/lib/screens/navigational_base_screen/navigational_base_screen.dart
+++ rankmyroast/lib/screens/navigational_base_screen/navigational_base_screen.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:rankmyroast/screens/navigational_base_screen/views/calendar/calendar_view.dart';
 import 'package:rankmyroast/screens/navigational_base_screen/views/groups/groups_view.dart';
 import 'package:rankmyroast/screens/navigational_base_screen/views/home/home_view.dart';
+import 'package:rankmyroast/screens/navigational_base_screen/views/recipe/recipe_view.dart';
 import 'package:rankmyroast/screens/navigational_base_screen/widgets/create_username_dialog_widget.dart';
 import 'package:rankmyroast/screens/navigational_base_screen/widgets/sign_out_dialog_widget.dart';
 import 'package:rankmyroast/services/supabase_helper.dart';
@@ -31,12 +32,17 @@ class _NavigationalBaseScreenState extends State<NavigationalBaseScreen> {
       label: "Calendar",
       icon: Icon(Icons.calendar_month, color: Colors.white),
     ),
+    BottomNavigationBarItem(
+      label: "Recipe",
+      icon: Icon(Icons.restaurant, color: Colors.white),
+    ),
   ];
 
   final List<Widget> navigationalViews = [
     GroupsView(),
     HomeView(),
     CalendarView(),
+    RecipeView(),
   ];
 
   @override
@@ -67,6 +73,7 @@ class _NavigationalBaseScreenState extends State<NavigationalBaseScreen> {
       ),
       body: navigationalViews[navigationalIndex],
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.green,
         showSelectedLabels: false,
         showUnselectedLabels: false,
