740db7740a6c22fc41965716333509eb721d88c5
James Mata <jamesmata556@gmail.com>
2026-03-24 13:18:49 -0400

    Image uploading works

--- rankmyroast/lib/screens/navigational_base_screen/views/recipe/recipe_view.dart
+++ rankmyroast/lib/screens/navigational_base_screen/views/recipe/recipe_view.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:rankmyroast/services/supabase_helper.dart';
 
 class RecipeView extends StatefulWidget {
   const RecipeView({super.key});
@@ -10,6 +14,30 @@ class RecipeView extends StatefulWidget {
 class _RecipeViewState extends State<RecipeView> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Recipe"));
+    return Center(
+      child: ElevatedButton(
+        onPressed: () async {
+          final image = await _takePhoto();
+          if (image != null) {
+            await _uploadPhoto(image);
+          }
+        },
+        child: Text("Test"),
+      ),
+    );
+  }
+
+  Future<File?> _takePhoto() async {
+    final image = SupabaseHelper.storage.pickImage(ImageSource.camera);
+    return image;
+  }
+
+  Future<void> _uploadPhoto(File file) async {
+    await SupabaseHelper.storage.uploadFileToFolder(
+      bucketName: "recipe_image",
+      folderName: "2d24a4c0-479f-4656-9778-2e8ab2dce3e6",
+      file: file,
+      fileName: "test.png",
+    );
   }
 }
--- rankmyroast/lib/services/modules/supabase_helper_storage.dart
+++ rankmyroast/lib/services/modules/supabase_helper_storage.dart
@@ -1,8 +1,40 @@
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseHelperStorage {
   static final _client = Supabase.instance.client;
+  static final ImagePicker _picker = ImagePicker();
+
+  Future<File?> pickImage(ImageSource source) async {
+    // 1. Determine which permission to check
+    Permission permission =
+        (source == ImageSource.camera)
+            ? Permission.camera
+            : (Platform.isIOS ? Permission.photos : Permission.storage);
+
+    // 2. Request/Check Permission
+    PermissionStatus status = await permission.request();
+
+    if (status.isGranted) {
+      // 3. Pick the Image
+      final XFile? pickedFile = await _picker.pickImage(
+        source: source,
+        imageQuality: 80, // Integrated compression
+        maxWidth: 1024, // Resize on the fly
+      );
+
+      if (pickedFile != null) {
+        return File(pickedFile.path);
+      }
+    } else if (status.isPermanentlyDenied) {
+      // User rejected multiple times; they must go to settings manually
+      await openAppSettings();
+    }
+
+    return null;
+  }
 
   Future<void> uploadFileToFolder({
     required String bucketName,
