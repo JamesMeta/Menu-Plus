d9a7c2e2e5351200da74b5d800501c34dd0f2935
James Mata <jamesmata556@gmail.com>
2026-04-13 13:05:22 -0400

    edit recipe database function completed

--- rankmyroast/lib/services/modules/supabase_helper_recipe.dart
+++ rankmyroast/lib/services/modules/supabase_helper_recipe.dart
@@ -4,6 +4,8 @@ import 'package:rankmyroast/models/group.dart';
 import 'package:rankmyroast/models/responses/create_recipe_response.dart';
 import 'package:rankmyroast/services/supabase_helper.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/v4.dart';
 
 class SupabaseHelperRecipe {
   static final _client = Supabase.instance.client;
@@ -44,6 +46,100 @@ class SupabaseHelperRecipe {
       }
 
       final success = true;
+
+      final List<Map<String, dynamic>> groupLinks =
+          groupList
+              .map((g) => {"recipe_id": recipeId, "group_id": g.id})
+              .toList();
+
+      final failedGroups = <Group>[];
+
+      try {
+        await _client.from("recipe_group").insert(groupLinks);
+      } catch (e) {
+        failedGroups.addAll(groupList);
+      }
+
+      bool imageUploadFailed = false;
+      if (image != null) {
+        final imageResponse = await SupabaseHelper.storage.uploadFileToBucket(
+          bucketName: "public_recipe_image",
+          file: image,
+          fileName: imageName,
+        );
+
+        if (imageResponse == null) imageUploadFailed = true;
+      }
+      return CreateRecipeResponse(
+        success: success,
+        failedToAddGroups: failedGroups,
+        failedToUploadImage: imageUploadFailed,
+      );
+    } catch (e) {
+      return CreateRecipeResponse(
+        success: false,
+        localError: false,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  String generateUUID() {
+    final uuid = Uuid();
+
+    return uuid.v4();
+  }
+
+  Future<CreateRecipeResponse> updateRecipe(
+    File? image,
+    String name,
+    List<String>? ingredientList,
+    List<String>? instructionList,
+    List<String>? groceryList,
+    List<Group> groupList,
+    bool? isPublic,
+  ) async {
+    try {
+      final newImageName = generateUUID();
+
+      final response =
+          await _client
+              .from("recipe")
+              .update({
+                "name": name,
+                "ingredients": ingredientList,
+                "instructions": instructionList,
+                "groceries": groceryList,
+                "image_name": newImageName,
+                "is_public": isPublic,
+              })
+              .select("*")
+              .single();
+
+      final recipeId = response["id"];
+      final imageName = response["image_name"];
+
+      if (imageName == null) {
+        return CreateRecipeResponse(
+          success: false,
+          localError: true,
+          errorMessage:
+              "Error: Recipe failed to be updated at this time, please try again later.",
+        );
+      }
+
+      if (imageName != newImageName) {
+        return CreateRecipeResponse(
+          success: false,
+          localError: true,
+          errorMessage:
+              "Error: Recipe failed to be updated at this time, please try again later.",
+        );
+      }
+
+      final success = true;
+
+      await _client.from("recipe_group").delete().eq("recipe_id", recipeId);
 
       final List<Map<String, dynamic>> groupLinks =
           groupList
