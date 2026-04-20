8f38cc927d58e5e4239fe21b93f73de8aa356316
James Mata <jamesmata556@gmail.com>
2026-04-20 14:19:20 -0400

    not in the mood

--- rankmyroast/lib/screens/navigational_base_screen/views/recipe/screens/viewer/recipe_viewer.dart
+++ rankmyroast/lib/screens/navigational_base_screen/views/recipe/screens/viewer/recipe_viewer.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rankmyroast/classes/modals/recipe.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -50,33 +51,146 @@ class _RecipeViewerState extends State<RecipeViewer> {
         ],
       ),
       body: SafeArea(
-        child: Center(
+        child: SingleChildScrollView(
           child: Container(
-            decoration: BoxDecoration(color: Colors.white),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(24),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(115, 0, 0, 0),
+                  blurRadius: 10,
+                  offset: Offset(2, 5),
+                ),
+              ],
+            ),
             child: Column(
               mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Expanded(
-                  child:
-                      _recipeImageUrl != null
-                          ? CachedNetworkImage(
-                            httpHeaders: {
-                              'Authorization':
-                                  'Bearer ${Supabase.instance.client.auth.currentSession?.accessToken}',
-                            },
-                            imageUrl: _recipeImageUrl,
-                            fit: BoxFit.fitWidth,
-                            placeholder:
-                                (context, url) =>
-                                    const CircularProgressIndicator(),
-                            errorWidget:
-                                (context, url, error) =>
-                                    const Icon(Icons.error),
-                          )
-                          : Image.asset("assets/images/rankmyroast_icon4.png"),
-                ),
+                _recipeImageUrl != null
+                    ? Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      child: CachedNetworkImage(
+                        httpHeaders: {
+                          'Authorization':
+                              'Bearer ${Supabase.instance.client.auth.currentSession?.accessToken}',
+                        },
+                        imageUrl: _recipeImageUrl,
+                        scale: 1,
+                        placeholder:
+                            (context, url) => const CircularProgressIndicator(),
+                        errorWidget:
+                            (context, url, error) => const Icon(Icons.error),
+                      ),
+                    )
+                    : Image.asset(
+                      "assets/images/rankmyroast_icon4.png",
+                      scale: 1,
+                    ),
 
-                Text("Recipe"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        _recipe.name,
+                        style: TextStyle(
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      Text(
+                        "Reviews",
+                        style: TextStyle(
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          border: Border.all(color: Colors.grey[600]!),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Estimated Preparation Time: ",
+                                    style: TextStyle(
+                                      fontSize: 18.sp,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+
+                                  Text(
+                                    "???",
+                                    style: TextStyle(
+                                      fontSize: 18.sp,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Estimated Cook Time: ",
+                                    style: TextStyle(
+                                      fontSize: 18.sp,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+
+                                  Text(
+                                    "???",
+                                    style: TextStyle(
+                                      fontSize: 18.sp,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Ingredients",
+                        style: TextStyle(
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      Text(
+                        "Instructions",
+                        style: TextStyle(
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
