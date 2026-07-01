acc6d277552be2891c50c1c188374391bc7a120e
James Mata <jamesmata556@gmail.com>
2026-07-01 12:15:56 -0400

    Supabase APIs setup

--- rankmyroast/lib/screens/navigational_base_screen/views/calendar/schedule_view.dart
+++ rankmyroast/lib/screens/navigational_base_screen/views/calendar/schedule_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rankmyroast/classes/modals/schedule.dart';
+import 'package:rankmyroast/services/supabase_helper.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class ScheduleView extends StatefulWidget {
@@ -9,9 +12,13 @@ class ScheduleView extends StatefulWidget {
 }
 
 class _ScheduleViewState extends State<ScheduleView> {
+  late Future<List<Schedule>?> _scheduledEvents;
+
   @override
   void initState() {
     super.initState();
+
+    _scheduledEvents = _getEvents();
   }
 
   @override
@@ -21,7 +28,130 @@ class _ScheduleViewState extends State<ScheduleView> {
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: SfCalendar(view: CalendarView.month),
+      child: Column(
+        children: [
+          FutureBuilder(
+            future: _scheduledEvents,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Active Groups",
+                          style: TextStyle(
+                            fontSize: 28.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "No active groups found",
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Expanded(child: SizedBox()),
+                    IconButton(
+                      onPressed: () {
+                        //TODO
+                        // create scheduled event
+                      },
+                      icon: Icon(Icons.add, color: Colors.white, size: 22.sp),
+                      constraints: BoxConstraints(
+                        minWidth: 40.w,
+                        minHeight: 40.w,
+                      ),
+                      style: IconButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                          side: BorderSide(color: Colors.transparent, width: 1),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }
+
+              if (snapshot.hasError) {
+                return Text("Error: ${snapshot.error}");
+              }
+
+              if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return SizedBox();
+              } else {
+                return Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Active Groups ",
+                          style: TextStyle(
+                            fontSize: 28.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "${snapshot.data!.length} group(s) found",
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                            SizedBox(width: 2),
+                            GestureDetector(
+                              onTap:
+                                  () => setState(() {
+                                    _scheduledEvents = _getEvents();
+                                  }),
+                              child: Icon(
+                                Icons.refresh_rounded,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Expanded(child: SizedBox()),
+                    IconButton(
+                      onPressed: () {
+                        //TODO
+                        // create scheduled event
+                      },
+                      icon: Icon(Icons.add, color: Colors.white, size: 22.sp),
+                      constraints: BoxConstraints(
+                        minWidth: 40.w,
+                        minHeight: 40.w,
+                      ),
+                      style: IconButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                          side: BorderSide(color: Colors.transparent, width: 1),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }
+            },
+          ),
+          SfCalendar(view: CalendarView.month),
+        ],
+      ),
     );
+  }
+
+  Future<List<Schedule>?> _getEvents() async {
+    return await SupabaseHelper.schedule.getAllScheduledEventsForUser();
   }
 }
